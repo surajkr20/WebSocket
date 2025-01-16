cbd9937c6093012f069b7ca612596c4bf271f2b1
surajkr20 <suraj71308kumar@gmail.com>
2025-01-16 14:29:51 +0530

    socket io setup at server-side

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,29 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const app = express();
-const port = 7000; // Define the port once and reuse
+const port = 7000; // Define the port
+
+// Create an HTTP server
+const server = createServer(app);
+
+// Attach Socket.IO to the server
+const io = new Server(server);
 
 // Define the root route
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-// Start the server
-app.listen(port, () => {
+// Handle Socket.IO connections
+io.on('connection', (socket) => {
+  console.log('User connected');
+  console.log('User ID:', socket.id);
+    
+});
+
+// Start the HTTP server
+server.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
